6532ab5ed4969a90d177552597cc64cb4fdee52d
Sumit Kushwah <sumitkushwah1729@gmail.com>
2024-02-22 07:12:51 +0530

    changes for master

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func Info() string {
-	return "This is my library and branch is prod."
+	return "This is my library and branch is master."
 }
 
 func main() {
-	fmt.Println("This is my library and branch is prod.")
+	fmt.Println("This is my library and branch is master.")
 }
